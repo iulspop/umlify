78a4fb3e5424078bd7b58301e5c650322f9158f8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-12-11 15:56:22 -0500

    comment out http call

--- lib/umlify/runner.rb
+++ lib/umlify/runner.rb
@@ -35,11 +35,12 @@ module Umlify
             classes.each {|c| add c}
           end.compute!
 
-          image = download_image(@diagram.get_uri)
-          save_to_file image
+          # image = download_image(@diagram.get_uri)
+          # save_to_file image
 
-          puts 'http://yuml.me'+@diagram.get_uri if @html_mode
-          puts "Saved in uml.png."
+          # puts 'http://yuml.me'+@diagram.get_uri if @html_mode
+          # puts "Saved in uml.png."
+          p @diagram
         else
           puts "No ruby files in the directory."
         end
@@ -56,31 +57,31 @@ module Umlify
     end
 
     # Downloads the image of the uml diagram from yUML
-    def download_image uri
-      connection = Net::HTTP
-      if ENV["HTTP_PROXY"] != nil 
-    	  proxy_host = ENV["HTTP_PROXY"].split(":")[0]
-    	  proxy_port = ENV["HTTP_PROXY"].split(":")[1]
-    	  proxy_user = ENV["HTTP_PROXY_USER"]
-    	  proxy_pass = ENV["HTTP_PROXY_PASS"]
-    		connection = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass)
-    	end
-      res = connection.post_form(URI("http://yuml.me/diagram/scruffy/class/"), {"dsl_text"=>@diagram.get_dsl})
-      puts res.body
-      url = "http://yuml.me/#{res.body.strip()}"
-      puts url
-      #Net::HTTP.get(URI(url))
-      connection.start("yuml.me", 80) do |http|
-        http.get(URI.decode("/#{res.body}"))        
-      end
-    end
+    # def download_image uri
+    #   connection = Net::HTTP
+    #   if ENV["HTTP_PROXY"] != nil 
+    # 	  proxy_host = ENV["HTTP_PROXY"].split(":")[0]
+    # 	  proxy_port = ENV["HTTP_PROXY"].split(":")[1]
+    # 	  proxy_user = ENV["HTTP_PROXY_USER"]
+    # 	  proxy_pass = ENV["HTTP_PROXY_PASS"]
+    # 		connection = Net::HTTP::Proxy(proxy_host, proxy_port, proxy_user, proxy_pass)
+    # 	end
+    #   res = connection.post_form(URI("http://yuml.me/diagram/scruffy/class/"), {"dsl_text"=>@diagram.get_dsl})
+    #   puts res.body
+    #   url = "http://yuml.me/#{res.body.strip()}"
+    #   puts url
+    #   #Net::HTTP.get(URI(url))
+    #   connection.start("yuml.me", 80) do |http|
+    #     http.get(URI.decode("/#{res.body}"))        
+    #   end
+    # end
 
     #Saves the diagram to file
-    def save_to_file image
-      File.open('uml.png', 'wb') do |file|
-        file << image.body
-      end if image
-    end
+    # def save_to_file image
+    #   File.open('uml.png', 'wb') do |file|
+    #     file << image.body
+    #   end if image
+    # end
 
   end
 end
