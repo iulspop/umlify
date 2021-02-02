d09d841b8076d9c1cfaff527c9969c36865994bf
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-02-02 11:59:04 -0500

    Add save to svg

--- lib/umlify/runner.rb
+++ lib/umlify/runner.rb
@@ -34,10 +34,9 @@ module Umlify
             classes.each { |c| add c }
           end.compute!
 
-          # puts diagram.statements.join("\n")
-          image = download_image(diagram)
-          # save_to_file(image)
-          # puts "Saved in uml.png."
+          svg = download_svg(diagram)
+          save_to_file(svg)
+          puts "Saved in uml.svg"
         else
           puts 'No ruby files in the directory.'
         end
@@ -52,18 +51,18 @@ module Umlify
       end.parse! @args
     end
 
-    def download_image(diagram)
+    def download_svg(diagram)
       scheme = "https://"
       host = "yuml.me"
       path = "/diagram/scruffy/class/" + ERB::Util.url_encode(diagram.get_dsl)
       uri = URI(scheme + host + path)
-      Net::HTTP.get_response(uri)
+      Net::HTTP.get_response(uri).body
     end
 
-    # def save_to_file image
-    #   File.open('uml.png', 'wb') do |file|
-    #     file << image.body
-    #   end
-    # end
+    def save_to_file(svg)
+      File.open('uml.svg', 'w') do |file|
+        file << svg
+      end
+    end
   end
 end
