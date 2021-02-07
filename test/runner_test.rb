94a25db5dd14f92d20243f009acd8d875f2a25f4
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-02-06 19:09:57 -0500

    Quick commit

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -6,23 +6,23 @@ class RunnerTest < Minitest::Test
 
   context "Runner" do
 
-    should 'be in smart mode when passed -s or --smart as an option'  do
-      r = RubyToUML::Runner.new(["-s"])
-      r.run
-      assert r.smart_mode
-      r = RubyToUML::Runner.new(["--smart"])
-      r.run
-      assert r.smart_mode
-    end
+    # should 'be in smart mode when passed -s or --smart as an option'  do
+    #   r = RubyToUML::Runner.new(["-s"])
+    #   r.run
+    #   assert r.smart_mode
+    #   r = RubyToUML::Runner.new(["--smart"])
+    #   r.run
+    #   assert r.smart_mode
+    # end
 
-    should "print the api url when passed -h or --html option"  do
-      r = RubyToUML::Runner.new(["-h"])
-      r.run
-      assert r.html_mode
-      r = RubyToUML::Runner.new(["--html"])
-      r.run
-      assert r.html_mode
-    end
+    # should "print the api url when passed -h or --html option"  do
+    #   r = RubyToUML::Runner.new(["-h"])
+    #   r.run
+    #   assert r.html_mode
+    #   r = RubyToUML::Runner.new(["--html"])
+    #   r.run
+    #   assert r.html_mode
+    # end
   end
 end
 
